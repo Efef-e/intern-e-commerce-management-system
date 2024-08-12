022b50ebac3d2bf800684763a99778ef25422421
Efef-e <emirhanefe458@gmail.com>
2024-08-12 17:05:32 +0300

    h3 black text

--- intern-tasking/src/app/productlist/page.tsx
+++ intern-tasking/src/app/productlist/page.tsx
@@ -119,7 +119,7 @@ const ProductList = () => {
                   key={product.id}
                   className="bg-white border border-gray-200 rounded-lg shadow-md p-4"
                 >
-                  <h3 className="text-lg font-semibold mb-2">
+                  <h3 className="text-black text-lg font-semibold mb-2">
                     {product.name}
                   </h3>
                   <p className="text-gray-600">
